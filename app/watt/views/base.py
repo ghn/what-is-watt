c59c4af7465a32e044b0966daac69800b219e8e0
Germain <germain.hugon@liip.ch>
2019-02-15 15:23:37 +0100

    Basic form with items

--- app/watt/views/base.py
+++ app/watt/views/base.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from app.watt.models import Item
+
 
 def index(request):
-    return render(request, 'index.html')
+    value = 0
+
+    if request.method == 'POST':
+        item1 = Item.objects.get(id=request.POST.get('item1'))
+        item2 = Item.objects.get(id=request.POST.get('item2'))
+
+        # compute conversion
+        value = item1.id + item2.id
+
+    return render(request, 'index.html', {
+        'items': Item.objects.all(),
+        'item1': item1,
+        'item2': item2,
+        'value': value
+    })
