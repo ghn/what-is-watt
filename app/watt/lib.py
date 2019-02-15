daff60ed0b533715bf338ee0fb1c6320d9079a9b
Germain <germain.hugon@liip.ch>
2019-02-15 16:56:43 +0100

    Improve display + make the calculation almost right

--- app/watt/lib.py
+++ app/watt/lib.py
@@ -3,8 +3,7 @@ def compute_energy(item1, item2, item1_quantity=1):
     compute the amount of item2 for given quantity of item1
     """
 
-    # TODO Fix it
-    return item1_quantity + item1.id + item2.id
+    return round(item1_quantity * (item1.power / item2.power), 3)
 
 
 class ItemCompute(object):
