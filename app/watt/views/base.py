e214f1d20dc7db5ed4c185f831ff9cc9e8d09bb6
Germain <germain.hugon@liip.ch>
2019-02-15 15:36:31 +0100

    fix

--- app/watt/views/base.py
+++ app/watt/views/base.py
@@ -6,17 +6,19 @@ from app.watt.lib import compute_energy
 
 
 def index(request):
-    value = 0
-
     if request.method == 'POST':
         item1 = Item.objects.get(id=request.POST.get('item1'))
         item2 = Item.objects.get(id=request.POST.get('item2'))
 
         value = compute_energy(item1, item2)
 
+        return render(request, 'index.html', {
+            'items': Item.objects.all(),
+            'item1': item1,
+            'item2': item2,
+            'value': value
+        })
+
     return render(request, 'index.html', {
-        'items': Item.objects.all(),
-        'item1': item1,
-        'item2': item2,
-        'value': value
+        'items': Item.objects.all()
     })
