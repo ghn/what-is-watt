8e526776ae0c14eb458a30d826b9f9c49964fae6
Germain <germain.hugon@liip.ch>
2019-02-15 20:15:30 +0100

    Energy type is an enum

--- app/watt/models.py
+++ app/watt/models.py
@@ -1,13 +1,23 @@
 from django.db import models
+from django_enumfield import enum
+
+
+class EnergyType(enum.Enum):
+    ELECTRIC = 0
+
+    labels = {
+        ELECTRIC: 'Electrical'
+    }
 
 
 class ItemManager(models.Manager):
     pass
 
+
 class Item(models.Model):
     name = models.CharField(max_length=200)
     wh_per_unit = models.PositiveIntegerField()
-    energy_type = models.CharField(max_length=200)
+    energy_type = enum.EnumField(EnergyType, default=EnergyType.ELECTRIC)
     unit = models.CharField(max_length=10, null=True)
     default_usage = models.FloatField(default=1)
     source = models.CharField(max_length=512, null=True)
